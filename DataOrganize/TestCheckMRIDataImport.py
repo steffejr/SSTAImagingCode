bed056b8b0023e3e35bcbc1e9c6bd437dee1515d
Jason Steffener NCMLab2 <steffener@gmail.com>
2020-04-15 15:25:11 -0400

    minor changes

--- DataOrganize/TestCheckMRIDataImport.py
+++ DataOrganize/TestCheckMRIDataImport.py
@@ -1,6 +1,6 @@
 import glob
 i = AllImports[3]
-PartID = '1002043'
+PartID = '1002013'
 Visitid = 'V001'
 
 VisProcMRIFolder = os.path.join(BaseDir,"ProcMRIData",PartID,Visitid)
@@ -18,19 +18,6 @@ if os.path.exists(ExpectedFilePath):
 else:
     # Add functionality for what to do if it is missing!
     # FoundFiles = glob.glob(os.path.join(RawMRIFolder, '*'+i['SearchString']+'*'+i['Extension']))
-        filename = MRIDataImport.SelectOneFile(i, RawMRIFolder)
-        if len(filename) > 0:
-            # Create the name of the file
-            OutFileName =  "%s_%s_%s.%s"%(PartID,Visitid,i['FileNameTag'], i['Extension'])
-            # Create the output path for the file
-            # Add another parameter to the AllImports structure naming the ouput folder
-            # which allows multiple images to reconstruct to the same folder
-            OutFilePath = os.path.join(VisProcMRIFolder, i['FileNameTag'])
-            MRIDataImport.MoveFile(filename, OutFileName, OutFilePath, i['FileNameTag'])
+
 
     
-    
-
-
-RawMRIFolder = os.path.join(BaseDir,"RawMRIData",PartID,Visitid)
-glob.glob(os.path.join(RawMRIFolder, '*'+i['SearchString']+'*.nii'))
